8580aef6b1729c6619c1e8534e8f41c5da370e3f
KaMiila <lacielacie@rocketmail.com>
2012-11-10 21:17:18 -0800

    stable release

---
+++ boincgui.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Edit the following line!  This will set the working directory for Boinc. Use a full path and make sure your user has write access to that folder!
+
+workdir=
+
+#Sanity Check
+
+if [ ! $workdir ]; then
+    echo "Set up the client like the README told you to!"
+    exit 1
+fi
+
+#Now we will make the directory if it does not already exist
+mkdir -p $workdir
+boincmgr --clientdir=/usr/bin --datadir=$workdir
